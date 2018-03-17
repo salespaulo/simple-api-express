95f7bdb5cdde12bd97d6130c730754a2c546a820
salespaulo <salespaulo08@gmail.com>
2018-03-17 15:50:06 -0300

    Add public and *.txt to ignore file

--- public/.baseDir.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLmJhc2VEaXIuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvYXBwLy5iYXNlRGlyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
